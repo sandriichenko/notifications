45b36b85ad2e8751fc09d137acb3dfde33b7aa6c
Sofia Andriichenko <sandriichenko@mirantis.com>
2016-09-16 11:57:08 -0400

    Notifications works

--- baremetal_notification.py
+++ baremetal_notification.py
@@ -24,6 +24,7 @@ from oslo_messaging import transport
 from tempest.api.baremetal.admin.base import BaseBaremetalTest
 from oslo_messaging._drivers import common
 from ironic_tempest_plugin.tests.scenario import baremetal_manager
+from ironic_tempest_plugin import clients
 
 def get_url(conf):
     conf = conf.oslo_messaging_rabbit
@@ -35,16 +36,11 @@ class NotificationHandler(object):
     def __init__(self, stack_id, events=None):
         self._notifications = []
         self.stack_id = stack_id
-        self.events = events
 
     def process_message(self, body, message):
         notification = common.deserialize_msg(body)
-        if notification['payload']['stack_name'] == self.stack_id:
-            if self.events is not None:
-                if notification['event_type'] in self.events:
-                    self.notifications.append(notification['event_type'])
-            else:
-                self.notifications.append(notification['event_type'])
+        if notification['payload']["ironic_object.data"]['uuid'] == self.stack_id:
+            self.notifications.append(notification['event_type'])
         message.ack()
 
     def clear(self):
@@ -55,16 +51,12 @@ class NotificationHandler(object):
         return self._notifications
 
 
-
-
-
 class BaremetalNotifications(BaseBaremetalTest):
-
     def setUp(self):
         super(BaremetalNotifications, self).setUp()
         self.exchange = kombu.Exchange('ironic', 'topic', durable=False)
         queue = kombu.Queue(exchange=self.exchange,
-                            routing_key='notifications.info',
+                            routing_key='ironic_versioned_notifications.info',
                             exclusive=True)
         self.conn = kombu.Connection(get_url(
             transport.get_transport(cfg.CONF).conf))
@@ -72,8 +64,36 @@ class BaremetalNotifications(BaseBaremetalTest):
         self.queue = queue(self.ch)
         self.queue.declare()
 
+    @classmethod
+    def setup_clients(cls):
+        super(BaremetalNotifications, cls).setup_clients()
+        cls.baremetal_client = clients.Manager().baremetal_client
+
     def test_baremetal_server_ops(self):
         _, chassis = self.create_chassis()
-        uuid = chassis['uuid']
-        _, node = self.create_node(uuid)
-        print node
+        _, node = self.create_node(chassis['uuid'])
+
+        handler = NotificationHandler(node['uuid'])
+
+
+        #import pdb; pdb.set_trace()
+        #print node
+        chassis_uuid = 'd504b075-740e-4e56-9782-f8ed1093c8d0'
+        #print
+        ## print self.baremetal_client.set_node_provision_state(node['uuid'], manage)
+        self.baremetal_client.update_node(node['uuid'], add=chassis_uuid)
+        #print
+        #print node
+        handler = NotificationHandler(chassis['uuid'])
+        with self.conn.Consumer(self.queue,
+                                callbacks=[handler.process_message],
+                                auto_declare=False):
+            try:
+                while True:
+                    self.conn.drain_events(timeout=1)
+            except Exception:
+                pass
+
+        print handler.notifications
+        ## for n in BASIC_NOTIFICATIONS:
+        ##     self.assertIn(n, handler.notifications)
